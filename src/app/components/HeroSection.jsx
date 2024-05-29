f4510c63efc4e187893746d3871352ee432b53de
Sanjiv Vasta <77975756+SanjivVasta@users.noreply.github.com>
2024-05-29 18:25:23 +0100

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 1 }}
           className="col-span-7 place-self-center text-center sm:text-left"
         >
-          <h1 className="text-white mb-4 text-3xl md:text-5xl lg:text-6xl font-extrabold">
+          <h1 className="text-white mb-4 text-2xl md:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I&#39;m{" "}</span><br />
             <TypeAnimation
               sequence={[
